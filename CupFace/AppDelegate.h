f7115b1a1c274026654f747b29fa919055480795
Tomasz Zablocki <skalamandra@poczta.onet.pl>
2012-09-05 20:08:10 +0100

    Initial Commit

---
+++ CupFace/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CupFace
+//
+//  Created by TZ on 05/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CupFace/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CupFace
+//
+//  Created by TZ on 05/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
